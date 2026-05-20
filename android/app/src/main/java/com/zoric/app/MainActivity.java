384b356f375f375dd3bbb4967d0025a781d52b2e
NinjaGamerz509 <srahemat943@gmail.com>
2026-05-20 13:30:58 +0530

    Fix: force desktop layout in WebView

--- android/app/src/main/java/com/zoric/app/MainActivity.java
+++ android/app/src/main/java/com/zoric/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.zoric.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(false);
+        settings.setInitialScale(25);
+    }
+}
